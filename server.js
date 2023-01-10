877c94e961ede374f6267ccd80ccb6f128a2f397
ausipome <ausipome@gmail.com>
2023-01-10 20:46:31 +0000

    up

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const secure = require('ssl-express-www');
-
-const app = express();
-
-app.use(secure);
-
-const port = $PORT;
-app.listen(port, () => console.log('Server listening.'));
