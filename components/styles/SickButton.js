7a33e9a232102509d65fc6eb147c524cec3f8119
ausipome <ausipome@gmail.com>
2023-01-13 15:52:12 +0000

    up

--- components/styles/SickButton.js
+++ components/styles/SickButton.js
@@ -10,7 +10,6 @@ const SickButton = styled.button`
   font-weight: 500 !important;
   border: 0 !important;
   border-radius: 0 !important;
-  text-transform: uppercase;
   font-size: 2rem !important;
   padding: 0.8rem 1.5rem !important;
   transform: skew(-2deg) !important;
