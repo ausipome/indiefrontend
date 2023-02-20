18a68920d45d1a7507ecef247c0aae467bec5c07
ausipome <ausipome@gmail.com>
2023-02-20 12:40:09 +0000

    up

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
 export const prodEndpoint = `https://keystone.indiebubba.com/api/graphql`;
-export const perPage = 20;
+export const perPage = 21;
